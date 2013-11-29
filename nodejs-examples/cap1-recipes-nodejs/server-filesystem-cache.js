bd9370a0b7a0396914c366c2bd774e7777d28cbc
marcosnasp <marcos.nas.p@gmail.com>
2013-11-29 11:59:49 -0300

    Incluindo exemplo de cache para otimizar o carregamento de recursos do file system componente

---
+++ nodejs-examples/cap1-recipes-nodejs/server-filesystem-cache.js
@@ -0,0 +1,48 @@
+
+
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+
+var mimeTypes = {
+	'.js' : 'text/javascript',
+	'.html' : 'text/html',
+	'.css' : 'text/css'
+};
+
+var cache = {};
+function cacheAndDeliver(f, cb) {
+	if(!cache[f]) {
+		fs.readFile(f, function(err, data) {
+			if(!err) {
+				cache[f] = {content : data};
+			}
+			cb(err, data);
+		});
+		return;
+	}
+	console.log('loading ' + f + ' from cache');
+	cb(null, cache[f].content);
+};
+
+
+http.createServer(function(request, response) {
+	var lookup = path.basename(decodeURI(request.url)) || 'index.html',
+	f = 'content/' + lookup;
+
+	fs.exists(f, function(exists) {
+		if(exists) {
+			cacheAndDeliver(f, function(err, data) {
+				if(err) {
+					response.writeHead(500);
+					response.end('Server Error!');
+					return;
+				}
+
+				var headers = {'Content-type' : mimeTypes[path.extname(lookup)]};
+				response.writeHead(200, headers);
+				response.end(data);
+			});
+		}
+	});
+}).listen(8080);
